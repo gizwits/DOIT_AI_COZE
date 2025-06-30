c195adab731a5156c445a10e321b8dc0cd2c8483
GizwitsAIoT <GizwitsAIoT@noreply.gitcode.com>
2025-06-30 09:02:18 +0800

    修正 GIzwitsC2 串口 io

--- main/boards/gizwits-ai-c2/config.h
+++ main/boards/gizwits-ai-c2/config.h
@@ -7,8 +7,8 @@
 // #define BOOT_BUTTON_GPIO        GPIO_NUM_9
 
 
-#define CODEC_TX_GPIO           GPIO_NUM_10
-#define CODEC_RX_GPIO           GPIO_NUM_18
+#define CODEC_TX_GPIO           GPIO_NUM_18
+#define CODEC_RX_GPIO           GPIO_NUM_10
 #define BUILTIN_LED_GPIO        GPIO_NUM_2
 #define BUILTIN_LED_NUM         20
 #define BUILTIN_LED_STRIP_GPIO   GPIO_NUM_20
